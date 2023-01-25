70f2ccd3e5e848e628963c64ae3221b463338eca
Ryan Faulhaber <ryf@sent.as>
2023-01-25 18:01:40 -0500

    initial commit

---
+++ reel.el
@@ -0,0 +1,25 @@
+;;; reel.el --- Rust-based HTTP library for Emacs Lisp. -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Ryan Faulhaber
+;;
+;; Author: Ryan Faulhaber <ryf@sent.as>
+;; Maintainer: Ryan Faulhaber <ryf@sent.as>
+;; Created: January 25, 2023
+;; Modified: January 25, 2023
+;; Version: 0.0.1
+;; Keywords: lisp
+;; Homepage: https://github.com/ryan/reel
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Rust-based HTTP library for Emacs Lisp.
+;;
+;;; Code:
+
+
+
+(provide 'reel)
+;;; reel.el ends here
